8d5e5a98bf9c4fb9839abea6725f6352da275441
piotr-yuxuan <pdeboisset@enst.fr>
2015-12-08 10:55:17 +0100

    automation

--- tex2txt.sh
+++ tex2txt.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-pandoc --toc -f latex -t rst -s $1.tex -o $1.rst && pandoc --toc -f rst -t markdown -s $1.rst -o $1.txt && pandoc --toc -f latex -t markdown -s $1.tex -o $1.md && pandoc --toc -f latex -t docx -s thesis.tex -o thesis.docx
+# This script is to generated the thesis is several formats.
+
+# If no argument supplied, apply on thesis
+[ -z "$1" ] && filename="thesis" || filename="$1"
+
+# Go in the real directory of this script.
+dirpath=`dirname $(realpath $0)`
+cd $dirpath
+
+# Some conversions. Please beware the tex format is not the only source.
+if [ ! -f $filename.tex ];
+then
+    echo "No $filename.tex found in $dirpath, exiting."
+    exit -1
+else
+	pandoc --toc -f latex -t rst -s $filename.tex -o $filename.rst &> /dev/null
+	pandoc --toc -f rst -t markdown -s $filename.rst -o $filename.txt &> /dev/null
+	pandoc --toc -f latex -t markdown -s $filename.tex -o $filename.md &> /dev/null
+	pandoc --toc -f latex -t docx -s $filename.tex -o $filename.docx &> /dev/null
+fi

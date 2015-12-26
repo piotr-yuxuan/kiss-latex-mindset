e72e50e0e9754596a9d2720471b74e8a72199b49
piotr-yuxuan <pierre.deboisset@telecom-paristech.fr>
2015-12-26 06:36:55 -1000

    rebranding to TPT

--- tex2txt.sh
+++ tex2txt.sh
@@ -5,6 +5,8 @@
 # If no argument supplied, apply on thesis
 [ -z "$1" ] && filename="thesis" || filename="$1"
 
+args=" --parse-raw --toc "
+
 # Some conversions. Please beware the tex format is not the only source.
 if [ ! -f $filename.tex ];
 then
@@ -14,11 +16,10 @@ else
 	# Go in the real directory of this script (likely ../data).
 	dirpath=`dirname $(realpath $filename.tex)`
 	cd $dirpath
-
 	# Work with formats
-	pandoc --toc -f latex -t rst -s $filename.tex -o $filename.rst
-	pandoc --toc -f rst -t markdown -s $filename.rst -o $filename.txt
-	pandoc --toc -f latex -t markdown -s $filename.tex -o $filename.md
-	pandoc --toc -f latex -t docx -s $filename.tex -o $filename.docx
-	pandoc --toc -f latex -t epub -s thesis.tex -o thesis.epub
+	pandoc $args -f latex -t rst -s $filename.tex -o $filename.rst
+	pandoc $args -f rst -t markdown -s $filename.rst -o $filename.txt
+	pandoc $args -f latex -t markdown -s $filename.tex -o $filename.md
+	pandoc $args -f latex -t docx -s $filename.tex -o $filename.docx
+	pandoc $args -f latex -t epub -s thesis.tex -o thesis.epub
 fi
